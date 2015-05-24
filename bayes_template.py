85fc89d32ec931c1a2f3fbebee9c50143c6fa4ea
friendly-flame <armsnyder@gmail.com>
2015-05-24 17:46:30 -0500

    Re-formatted and commented

--- bayes_template.py
+++ bayes_template.py
@@ -1,21 +1,24 @@
-# Name: 
-# Date:
-# Description:
+# Name: Michael Nowakowski (mjn912), Adam Snyder (ars123) and Steven Vorbrich (slv398)
+# Date: 24 May 2015
+# Description: Naive Bayes Classifier
 #
 #
 
-import math, os, pickle, re, json
+import math
+import os
+import pickle
+import json
 import nltk
 import json_database
-from nltk.tokenize import RegexpTokenizer
 
 
-class Bayes_Classifier:
+class BayesClassifier:
     def __init__(self):
         """This method initializes and trains the Naive Bayes Sentiment Classifier.  If a
         cache of a trained classifier has been stored, it loads this cache.  Otherwise,
         the system will proceed through training.  After running this method, the classifier
         is ready to classify input text."""
+
         self.pos_freq = load_pickle('pos_freq.p')
         self.neg_freq = load_pickle('neg_freq.p')
         self.pos_pres = load_pickle('pos_pres.p')
@@ -24,43 +27,25 @@ class Bayes_Classifier:
     def train(self):
         """Trains the Naive Bayes Sentiment Classifier."""
 
+        # If we have pickle files, there is no need to train
         if self.pos_freq:
-            return
+            return 1
 
         # Open the database
-        # Tokenize each set of text
-
-        total_words = []
-        # Go through all positive and negative reviews
         data = json_database.load_json_database()
-        #for review in data:
-        #    review = json.loads(review)
-        #    #print review['text']
-        #    for word in review['text'].split(' '):
-        #        if word not in total_words:
-        #            total_words.append(word)
 
-        #for word in total_words:
-        #    self.pos_freq[word] = 0.0
-        #    self.neg_freq[word] = 0.0
-        #    self.pos_pres[word] = 0.0
-        #    self.neg_pres[word] = 0.0
-
-        # Go through all files
-        pos_word_counter = 0   #Count the total number of words and documents
+        # Initialize counters for words and documents
+        pos_word_counter = 0
         neg_word_counter = 0
-        pos_review_counter = 0   #Count the total number of words and documents
+        pos_review_counter = 0
         neg_review_counter = 0
 
-
         for review in data:
-            # get text and store in string
+            # Get text and store in string
             review = json.loads(review)
-            text = review['text']
-            #words = self.tokenize(text)
-            words = nltk.word_tokenize(text)
-            words = [w.lower() for w in words]
-            # print words
+            words = [word.lower() for word in nltk.word_tokenize(review['text'])]
+
+            # Increment counters
             if review['status'] == '5':
                 pos_word_counter += len(words)
                 pos_review_counter += 1
@@ -81,6 +66,7 @@ class Bayes_Classifier:
                         self.neg_freq[word] += 1
                     else:
                         self.neg_freq[word] = 1.0
+
             # Presence
             if review['status'] == '5':
                 while len(words) > 0:
@@ -101,8 +87,7 @@ class Bayes_Classifier:
                     while word in words:
                         words.remove(word)
 
-
-        # Normalize the counts after going through all the files
+        # Normalize the counts after going through all the files and apply add-one smoothing
         for a in self.pos_freq:
             self.pos_freq[a] += 1
             self.pos_freq[a] /= (pos_word_counter+len(self.pos_pres))
@@ -116,57 +101,39 @@ class Bayes_Classifier:
             self.neg_pres[a] += 1
             self.neg_pres[a] /= (neg_review_counter+len(self.neg_pres))
 
-
-
-        print 'Positive Frequency:'
-        print [self.pos_freq[i] for i in self.pos_freq if self.pos_freq[i] != 0.0]
-        print 'Negative Frequency:'
-        print [self.neg_freq[i] for i in self.neg_freq if self.neg_freq[i] != 0.0]
-        print 'Positive Presence:'
-        print [self.pos_pres[i] for i in self.pos_pres if self.pos_pres[i] != 0.0]
-        print 'Negative Presence:'
-        print [self.neg_pres[i] for i in self.neg_pres if self.neg_pres[i] != 0.0]
-
-
+        # Save the training data with pickle
         save_pickle(self.pos_freq, 'pos_freq.p')
         save_pickle(self.neg_freq, 'neg_freq.p')
         save_pickle(self.pos_pres, 'pos_pres.p')
         save_pickle(self.neg_pres, 'neg_pres.p')
 
         return 1
-
     
-    def classify(self, sText):
+    def classify(self, s_text):
         """Given a target string sText, this function returns the most likely document
         class to which the target string belongs (i.e., positive, negative or neutral).
         """
-        # self.pos_pres is number of pos documents that contain given word divided by number of pos documents
-        # self.pos_freq =number of times a word appears in all the positive documents divided by total words in pos docs
+        # self.pos_pres: number of pos documents that contain given word divided by number of pos documents
+        # self.pos_freq: number of times a word appears in all the positive documents divided by total words in pos docs
         # take in text, tokenize it
         # check how likely words used are to be positive
         # check how likely it is words used are negative
 
-        word_tokens = nltk.word_tokenize(sText)
-        word_tokens = [w.lower() for w in word_tokens if w.isalpha()]
+        words = [word.lower() for word in nltk.word_tokenize(s_text)]
 
-        tokens_length = len(word_tokens)
+        # Initialize probabilities to 0
         pos_sum = 0
         neg_sum = 0
-        pos_miss = 0
-        neg_miss = 0
 
-        for token in word_tokens:
+        # Add the logs of the probabilities (an alternative to multiplying the probabilities)
+        for token in words:
             try:
                 pos_sum += (math.log10(self.pos_pres[token]))
                 neg_sum += (math.log10(self.neg_pres[token]))
             except KeyError:
-                pass
+                pass  # The word was not in our training data, so we ignore it
 
-        # print pos_sum, neg_sum
-
-        # pos_sum *= (1.0*(tokens_length-pos_miss)/tokens_length)
-        # neg_sum *= (1.0*(tokens_length-neg_miss)/tokens_length)
-        pos_sum-neg_sum
+        # Declare the sentiment
         if abs(pos_sum-neg_sum) < 1.5:
             return 'neutral'
         if pos_sum > neg_sum:
@@ -175,85 +142,28 @@ class Bayes_Classifier:
             return 'negative'
 
 
-    def loadFile(self, sFilename):
-        """Given a file name, return the contents of the file as a string."""
-
-        f = open(sFilename, "r")
-        sTxt = f.read()
-        f.close()
-        return sTxt
-   
-    def save(self, dObj, sFilename):
-        """Given an object and a file name, write the object to the file using pickle."""
-
-        f = open(sFilename, "w")
-        p = pickle.Pickler(f)
-        p.dump(dObj)
-        f.close()
-   
-    def load(self, sFilename):
-        """Given a file name, load and return the object stored in the file."""
-
-        f = open(sFilename, "r")
-        u = pickle.Unpickler(f)
-        dObj = u.load()
-        f.close()
-        return dObj
-
-    def tokenize(self, sText):
-        """Given a string of text sText, returns a list of the individual tokens that
-        occur in that string (in order)."""
-
-        lTokens = []
-        sToken = ""
-        for c in sText:
-            if re.match("[a-zA-Z0-9]", str(c)) != None or c == "\"" or c == "_" or c == "-":
-                sToken += c
-            else:
-                if sToken != "":
-                    lTokens.append(sToken)
-                    sToken = ""
-                if c.strip() != "":
-                    lTokens.append(str(c.strip()))
-               
-        if sToken != "":
-            lTokens.append(sToken)
-
-        return lTokens
-
-
-def freed_tokenize(sText):
-      """Given a string of text sText, returns a list of the individual tokens that
-      occur in that string (in order)."""
-
-      lTokens = []
-      sToken = ""
-      for c in sText:
-         if re.match("[a-zA-Z0-9]", str(c)) != None or c == "\"" or c == "_" or c == "-":
-            sToken += c
-         else:
-            if sToken != "":
-               lTokens.append(sToken)
-               sToken = ""
-            if c.strip() != "":
-               lTokens.append(str(c.strip()))
-
-      if sToken != "":
-         lTokens.append(sToken)
-
-      return lTokens
-
+# Custom helper functions:
 
 def load_pickle(filename):
+    """
+    Load a pickle and return the object
+    :param filename: File path for desired pickle
+    :return: Object
+    """
     model_file = relative_path(filename)
     if os.path.isfile(model_file):
         return pickle.load(open(model_file, 'rb'))
     return {}
 
 
-def save_pickle(object, filename):
+def save_pickle(object_to_save, filename):
+    """
+    Save an object as a pickle file
+    :param object_to_save: Object to save
+    :param filename: File destination
+    """
     model_file = open(relative_path(filename), 'wb')
-    pickle.dump(object, model_file)
+    pickle.dump(object_to_save, model_file)
     model_file.close()
 
 
--- tests.py
+++ tests.py
@@ -1,25 +1,12 @@
-__author__ = 'DoctorWatson'
 import unittest
-import json_database
-import json
-from bayes_template import Bayes_Classifier
-import math
+from bayes_template import BayesClassifier
+
 
 class TestClassifier(unittest.TestCase):
 
-    def setUp(cls):
-        cls.bayes = Bayes_Classifier()
-        cls.bayes.train()
-
-    def test_train(self):
-        bayes = Bayes_Classifier()
-        Bayes_Classifier.train(bayes)
-        # bayes.classify("Unstoppable and righteous, it roars across the no-lane hardpan like the four-iron horseman of "
-        #                "the kinetic apocalypse, amped up on bathtub crank and undiluted movie love. Oh, what a movie. "
-        #                "What a lovely movie!")
-        bayes.classify("It was definitely a movie.")
-        print math.log10(bayes.pos_pres["loved"])
-        print math.log10(bayes.neg_pres["loved"])
+    def setUp(self):
+        self.bayes = BayesClassifier()
+        self.bayes.train()
 
     def test_pos(self):
         self.assertEqual('positive', self.bayes.classify('I loved the movie.'))
@@ -29,10 +16,3 @@ class TestClassifier(unittest.TestCase):
 
     def test_newt(self):
         self.assertEqual('neutral', self.bayes.classify('Movies are movies.'))
-
-
-class TestDatabase(unittest.TestCase):
-
-    def testOutput(self):
-        json_data = json_database.load_json_database()
-
