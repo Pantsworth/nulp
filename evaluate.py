f8adc6d1cda1a73e51b90d9370dfa1c2e4f2e720
friendly-flame <armsnyder@gmail.com>
2015-05-24 23:26:50 -0500

    main

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,7 @@
+import json
+import bayesbest
+import itertools
+
 def segment_corpus(corpus, fold):
     """
     Given a corpus, segment it into a number of sub-corpora
@@ -6,3 +10,20 @@ def segment_corpus(corpus, fold):
     :return: array of data objects with size fold
     """
     pass
+
+
+def main():
+    fold = 10
+    with open(bayesbest.relative_path('data.json'), 'r') as f:
+        corpus = json.load(f)
+    sub_corpora = segment_corpus(corpus, fold)
+    for i in range(fold):
+        run_evaluation(itertools.chain(sub_corpora[:i]+sub_corpora[i:]), sub_corpora[i])
+
+
+def run_evaluation(training_data, test_data):
+    pass
+
+
+if __name__ == '__main__':
+    main()
