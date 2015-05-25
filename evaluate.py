b9aaa42d8070d3543f042af964b9c1b7b43b8628
friendly-flame <armsnyder@gmail.com>
2015-05-24 23:14:18 -0500

    stub

---
+++ evaluate.py
@@ -0,0 +1,8 @@
+def segment_corpus(corpus, fold):
+    """
+    Given a corpus, segment it into a number of sub-corpi
+    :param corpus: object containing data
+    :param fold: number of segments
+    :return: array of data objects with size fold
+    """
+    pass
