80551db0998c5dfc5bcfd1dfa3019c5abe76c856
svorbrich <svorbrich@gmail.com>
2015-05-24 23:16:38 -0500

    Corrected pluralization of corpus.

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,6 @@
 def segment_corpus(corpus, fold):
     """
-    Given a corpus, segment it into a number of sub-corpi
+    Given a corpus, segment it into a number of sub-corpora
     :param corpus: object containing data
     :param fold: number of segments
     :return: array of data objects with size fold
